1121c3e8207aaebcc6fc5fa162ba201967e275be
MaxElano <max.ferket2004@gmail.com>
2023-10-24 15:53:35 +0200

    Git Commit

--- app/Types.hs
+++ app/Types.hs
@@ -3,6 +3,7 @@
 module Types where
 
 import Prelude
+import GHC.Natural (Natural)
 
 type Board = [Row]
 type Row   = [Field]
@@ -18,3 +19,24 @@ instance Show FieldType where
     show PowerUp = "PowerUp"
     show Cherry = "Cherry"
     show Empty = "Empty"
+
+data PlayState = PlayState { board       :: Board
+			        	    ,score       :: Score
+	      	                ,pacman      :: PacMan
+                            ,ghostMode   :: GhostMode
+				            ,ghostRed    :: Ghost
+				            ,ghostPink   :: Ghost
+				            ,ghostCyan   :: Ghost
+				            ,ghostOrange :: Ghost
+	    			       }
+
+newtype Score = Score Natural
+
+data GhostMode = Chase | Scatter | Frightened
+
+data PacMan = PacMan { location           :: PacManLocation
+                      ,previousOrentation :: OldOrientation
+			          ,orientation        :: NewOrientation
+ 		              ,lives              :: Lives
+                      ,speed              :: Speed
+			         }
