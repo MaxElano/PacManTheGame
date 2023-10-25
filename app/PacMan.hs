397658cea9c66235b6c1a584a1f1434241c72d38
MaxElano <max.ferket2004@gmail.com>
2023-10-25 17:43:27 +0200

    weinig gedaan

---
+++ app/PacMan.hs
@@ -0,0 +1,6 @@
+{-# language CPP #-}
+{- This is a framework in which all functions to be written are "undefined".  -
+ - Note that in most cases parameters, pattern-matching and guards have been  -
+ - omitted! You will have to add those yourself.                              -}
+
+module PacMan where
