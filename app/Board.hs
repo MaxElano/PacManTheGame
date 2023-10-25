bb5dfffe92687e745c5fc165d6cecc9cd1d55534
DatDaniel1 <daniel.vanderhoeven2004@gmail.com>
2023-10-25 19:38:17 +0200

    Type changes and Board.hs creation

---
+++ app/Board.hs
@@ -0,0 +1,45 @@
+{-# language CPP #-}
+
+module Board where
+
+import Prelude
+import System.IO
+import Language.Haskell.TH (safe)
+import Data.Foldable (minimumBy)
+import Data.Ord (comparing)
+import Types
+    ( Orientation,
+      Location(..),
+      IsNotWall,
+      FieldType(Wall),
+      Field(..),
+      Row,
+      Board )
+
+locationToField :: Location -> Board -> Field
+locationToField l = findField l . findRow l
+
+findRow :: Location -> Board -> Row
+findRow (Location (_,y) _) board = let headRows = map (\r@((MkField (_,y1) _):_)-> (abs(fromIntegral y1 - y),r)) board 
+                                   in snd (minimumBy (comparing fst) headRows)
+
+findField :: Location -> Row -> Field
+findField (Location (x,_) _) row = let fields = map (\f@(MkField (x1,_) _) -> (abs(fromIntegral x1 - x),f)) row
+                                   in snd (minimumBy (comparing fst) fields)
+
+fieldToLocation :: Field -> Orientation -> Location
+fieldToLocation (MkField (x,y) _) = Location (fromIntegral x,fromIntegral y)
+
+wallCheck :: Field -> IsNotWall
+wallCheck (MkField _ Wall) = False
+wallCheck (MkField _ _)    = True
+
+changeFieldType :: Board -> Field -> FieldType -> Board
+changeFieldType board f newType = map (\row -> fieldReplace row f newType) board
+
+fieldReplace :: Row -> Field -> FieldType -> Row
+fieldReplace [] _ _ = []
+fieldReplace (f:fs) f1@(MkField pos _) newType = 
+    if f == f1
+        then MkField pos newType:fs
+        else f:fieldReplace fs f1 newType
--- app/Types.hs
+++ app/Types.hs
@@ -13,15 +13,31 @@ instance Show Field where
     show :: Field -> String
     show (MkField (x,y) s) = "(" ++ show x ++ "," ++ show y ++ ")" ++ show s
 
+instance Eq Field where
+    (==) :: Field -> Field -> Bool
+    MkField pos fieldType == MkField pos' fieldType' = pos == pos' && fieldType == fieldType'
+
 data FieldType = Wall | Pellet | PowerUp | Cherry | Empty
 instance Show FieldType where 
+    show :: FieldType -> String
     show Wall = "Wall"
     show Pellet = "Pellet"
     show PowerUp = "PowerUp"
     show Cherry = "Cherry"
     show Empty = "Empty"
 
-type FieldCoordinate = (Int, Int)
+instance Eq FieldType where
+    (==) :: FieldType -> FieldType -> Bool
+    Wall == Wall = True
+    Pellet == Pellet = True
+    PowerUp == PowerUp = True
+    Cherry == Cherry = True
+    Empty == Empty = True
+
+type CurrentField = Field
+type GhostLocation = Field
+type PacManLocation = Field
+type IsNotWall = Bool
 
 data PlayState = PlayState { board       :: Board
                             ,score       :: Score
@@ -33,7 +49,7 @@ data PlayState = PlayState { board       :: Board
                             ,ghostOrange :: Ghost
                            }
 
-newtype Score = MkScore Natural
+newtype Score = Score Natural
 
 data GhostMode = Chase | Scatter | Frightened
 
@@ -43,17 +59,30 @@ data PacMan = PacMan { location           :: Location
                       ,speed              :: Speed
                      }
 
-data Location = MkLocation (Float,Float) Orientation
-data Orientation = Up | Down | Left | Right
-newtype Lives = MkLives Natural
-newtype Speed = MkSpeed Natural
+data Location = Location (Float,Float) Orientation
 
-data Ghost = Ghost { location    :: Location 
+instance Show Location where
+    show :: Location -> String
+    show (Location (f,f') o) = "(" ++ show f ++ "," ++ show f' ++ ")" ++ show o
+
+data Orientation = Up | Down | Left | Right
+
+instance Show Orientation where
+    show :: Orientation -> String
+    show Up = "Up"
+    show Down = "Down"
+    show Types.Left = "Left"
+    show Types.Right = "Right"
+
+newtype Lives = Lives Natural
+newtype Speed = Speed Natural
+
+data Ghost = Ghost { glocation    :: Location 
                     ,targetField :: TargetField
-                    ,speed       :: Speed
+                    ,gspeed       :: Speed
                     ,baseField   :: BaseField
                     ,mustReverse :: MustReverse
-}
+                   }
 
 type MustReverse = Bool
 type BaseField = Field
